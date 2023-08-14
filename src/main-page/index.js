a4a5a3a55fc7770a5c48c89e3f0d97940440bb76
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2023-08-14 09:13:43 +0100

    hooks

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,15 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Header from "./header";
 import "./main-page.css";
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
   useEffect(() => {
     const fetchHouses = async () => {
       const resp = await fetch("/houses.json");
       const houses = await resp.json();
+      setAllHouses(houses);
     };
     fetchHouses();
   }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
 
   return (
     <div className="container">
