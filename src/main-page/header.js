cc9e49bdb7f4467e071b89bb2ce995de46fae2cc
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2023-08-14 08:36:48 +0100

    fetching the houses data json

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,15 +1,13 @@
 import React from "react";
 import logo from "./logo.png";
 
-const Header = () => {
+const Header = ({ subtitle }) => {
   return (
     <header className="row">
       <div className="col-md-5">
         <img src={logo} alt="logo" className="logo" />
       </div>
-      <div className="col-md-7 mt-5 subtitle">
-        Providing houses all over the world
-      </div>
+      <div className="col-md-7 mt-5 subtitle">{subtitle}</div>
     </header>
   );
 };
--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
 import Header from "./header";
 import "./main-page.css";
 
 function App() {
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const resp = await fetch("/houses.json");
+      const houses = await resp.json();
+    };
+    fetchHouses();
+  }, []);
+
   return (
     <div className="container">
-      <Header />
+      <Header subtitle="Providing houses all over the world" />
     </div>
   );
 }
