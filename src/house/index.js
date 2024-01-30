5e97b5d18f675b933449e825c868770268c3907d
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2024-01-30 13:05:43 +0100

    revisions

--- src/house/index.js
+++ src/house/index.js
@@ -13,6 +13,7 @@ const House = ({ house }) => {
       <div className="row mt-2">
         <h5 className="col-md-12">{house.country}</h5>
       </div>
+
       <div className="row">
         <h3 className="col-md-12">{house.address}</h3>
       </div>
